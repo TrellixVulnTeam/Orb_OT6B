2856a3f760f9ffe5269ba29acabc3af8bb860cd4
Zhuchen Zhong <1919121948@qq.com>
2021-04-04 23:44:52 +0800

    Updated bin/*

--- bin/OrbInstaller.py
+++ bin/OrbInstaller.py
@@ -9,9 +9,6 @@ Version Notes
     1.0
         First Commit
 '''
-from include.python.orbinstaller import init
-from include.python.orbinstaller.errtype import InitError
-import shelve
 from sys import platform as sysPlatform
 from sys import argv, exit
 from cmd import Cmd
@@ -19,16 +16,10 @@ import platform
 import os
 
 import colorama
-#import rich
 
 from orbinstaller import *
 from orbinstaller.errtype import *
-if not init():
-    '''
-    目前不会触发，现行错误直接抛出
-    这里留用
-    '''
-    exit(1) # 初始化失败
+
 
 # const
 argv = argv[:]
--- include/python/orbinstaller/__init__.py
+++ include/python/orbinstaller/__init__.py
@@ -7,24 +7,17 @@ Version Notes
     1.0
         First Commit
 '''
+__DEBUG = False
 __all__ = [
-    'PATH',
-    'raiseError',
-    'init'
+    'PATH'
 ]
 
 import os
-import shelve
-import time
-from copy import deepcopy as copy
 from sys import argv, exit
 argv = argv[:]
 
-import colorama
 
 from .errtype import *
-
-__DEBUG = False
 
 ROOT_PATH = os.sep.join(__file__.split(os.sep)[:-4])
 PATH = {
@@ -37,74 +30,3 @@ PATH = {
 }
 PATH['root'] = ROOT_PATH
 del ROOT_PATH
-configure = {}
-TMP = {}
-
-
-# tools
-def mkdir(name: str) -> bool:
-    if os.path.isdir(name):
-        return True
-    try:
-        os.mkdir(name)
-    except:
-        return False
-    else:
-        return True
-
-def raiseError(err: object, msg: str) -> None:
-    '''输出异常
-    :param err(str): 异常类
-    :param msg(str): 异常消息
-
-    :return(None)
-    '''
-    try:
-        colorama.init()
-    except NameError:  # rich version
-        pass
-    else:  # colorama version
-        raise err(
-            colorama.Fore.RED + colorama.Back.YELLOW +\
-                msg,
-            colorama.Fore.RESET + colorama.Back.RESET
-        )
-
-
-# function
-def set_mConfigure(key: str, fields: str = None, /, updateOnceWrite = True):
-    '''对主配置文件进行读写
-    :param key(str): 键
-    :param fields(str): 字段
-    :param updateOnceWrite: 写入后立即更新
-    '''
-    pass
-
-def get_mConfigure(key: str = '', /,useCache: bool = False, updateCache: bool = False):
-    '''获取主配置文件信息
-    :param key(str): 键
-    :param useCache(bool): 使用缓存
-    :param updateCache(bool): 若为真，其他动作自动失效，只更新缓存
-
-    :return(anyType)
-    '''
-    pass
-
-
-def init() -> bool:
-    '''执行初始化操作
-    :return(bool)
-    '''
-    if not mkdir(PATH['etc']):
-        raiseError(InitError, 'Fail to creat folder `%s`' % PATH['etc'])
-        return False
-    TMP['mConfigure_filename'] = os.path.join(PATH['etc'], 'configure')
-    
-    return True
-
-
-# test
-if __DEBUG:
-    from pprint import pprint
-    print("PATH:")
-    pprint(PATH)
