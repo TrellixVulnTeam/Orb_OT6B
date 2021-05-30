cbb32a2bd385f941f49ffee28c03bd03cb712b19
Zhuchen Zhong <1919121948@qq.com>
2021-05-30 23:35:15 +0800

    not yet

--- morc/core/server.py
+++ morc/core/server.py
@@ -1,8 +1,7 @@
-#socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 import socket
 import time
 from threading import Thread
 
-from .clientConnectionPool import *
 from .morclogger import *
 from .scode import *
+from .socer import *
---
+++ morc/core/socer.py
@@ -0,0 +1,12 @@
+'''
+异步socket框架
+
+包括连接池和普通的事件管理
+'''
+__all__ = ['SOCER', 'CONNPOOL']
+
+import socket
+import time
+
+
+
